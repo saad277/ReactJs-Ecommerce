74cab68ae5ae5361158fc26446defa8baeca346b
Saad Sheikh <saadsheikh27@gmail.com>
2020-06-30 11:58:29 +0500

    Updates

---
+++ precache-manifest.fc21e3fca0c5dfc68d60d73e26d1d8df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087cb0e129f881f837b95b845d89d541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92c88424ad0df464d23c",
+    "url": "/static/css/main.d7893c14.chunk.css"
+  },
+  {
+    "revision": "8b9e4049ac4d61ec3e6f",
+    "url": "/static/js/2.4489b452.chunk.js"
+  },
+  {
+    "revision": "92c88424ad0df464d23c",
+    "url": "/static/js/main.c59c415c.chunk.js"
+  },
+  {
+    "revision": "f84b125957fcf2744af9",
+    "url": "/static/js/runtime-main.c221e3d8.js"
+  },
+  {
+    "revision": "f4d611c52ea3561d652fd0d09ad2dc8b",
+    "url": "/static/media/iphone.f4d611c5.jpg"
+  },
+  {
+    "revision": "ca500e075906eeee5ddd2a58387fd3e9",
+    "url": "/static/media/pixel.ca500e07.jpg"
+  },
+  {
+    "revision": "e205ae33cdecd9a8c81d95873c5353e5",
+    "url": "/static/media/windows.e205ae33.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Module-Final/precache-manifest.7cbb5984e0da8419fa4fb574c4255df0.js"
+  "/precache-manifest.fc21e3fca0c5dfc68d60d73e26d1d8df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Module-Final/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
